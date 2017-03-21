0f3684d3a27bd81dda3140e5f396a260d492baaf
danny-sun <sunfei@dannymac.local>
2017-03-21 12:25:13 +0800

    Mail

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread 
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap 
 from flask_script import Manager, Shell
@@ -7,6 +8,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,11 +19,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 class Role(db.Model):
 	__tablename__ = 'roles'
@@ -40,6 +51,19 @@ class User(db.Model):
 	def __repr__(self):
 		return '<User %r>' % self.username
 
+def send_async_email(app, msg):
+	with app.app_context():
+		mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+	msg.body = render_template(template + '.txt', **kwargs)
+	msg.html = render_template(template + '.html', **kwargs)
+	thr = Thread(target=send_async_email, args=[app, msg])
+	thr.start()
+	return thr
+
 def make_shell_context():
 	return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
@@ -57,11 +81,14 @@ def index():
     		user = User(username = form.name.data)
     		db.session.add(user)
     		session['known'] = False
+    		if app.config['FLASKY_ADMIN']:
+    			send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+
     	else:
     		session['known'] = True
     	session['name'] = form.name.data
     	return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'), known = session.get('know', False))
 
 @app.route('/user/<name>')
 def user(name):
